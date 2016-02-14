0c374bfa2b53199e3c19d7d93d249be9f0dc5b95
Christian Alfoni <christianalfoni@gmail.com>
2016-02-14 20:37:06 +0100

    Updated deps and added copyForm

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,13 @@ module.exports = {
     loaders: [{
       test: /\.js?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query: {
+        "presets": ["react", "es2015", "stage-0"],
+        "plugins": [
+          ["transform-decorators-legacy"]
+        ]
+      }
     }, {
       test: /\.json?$/,
       loader: 'json'
