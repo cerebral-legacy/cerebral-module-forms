28b8fb0a1af06e73e002886c55bf996641bb7f81
Mathias Nilsson <wicket.programmer@gmail.com>
2016-04-02 14:53:51 +0200

    Update joinPath.js

--- helpers/joinPath.js
+++ helpers/joinPath.js
@@ -1,8 +1,8 @@
 module.exports = function joinPath (/*base, path1, path2, ... */) {
 
-  if( ! arguments[1] || ! 	Array.isArray(arguments[1]) ){
-  	console.warn( "arguments[1] dependent must be an array" );
-  	return;
+  if( ! arguments[1] || ! Array.isArray(arguments[1]) ) {
+    console.warn( "arguments[1] dependent must be an array" );
+    return;
   }
 	
   var baseIndex = arguments[1][0] === '.' || arguments[1][0] === '..' ? 0 : 1;
