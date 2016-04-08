492bf54bf14e4c21907fcce5dd0e79b07541f096
Javaprogrammer <info@edgesoft.se>
2016-04-08 15:27:06 +0200

    fix: rules[key] is not a function if you pass isValue with parameters
    fix: #14

--- helpers/hasValue.js
+++ helpers/hasValue.js
@@ -1,10 +1,19 @@
 var rules = require('./rules.js')
 
+function parseKey(value) {
+  if (typeof value === 'string') {
+    var args = value.split(/:(.+)?/)
+    return  args[1] ? {key: args[0], params: JSON.parse(args[1])} : {key: value}
+  }
+  return {key:value}
+}
+
 module.exports = function (form, value, isValueRules) {
   return isValueRules.reduce(function (isValue, key) {
     if (!isValue) {
       return false
     }
-    return rules[key](value, form)
+    var parsedKey = parseKey(key)
+    return rules[parsedKey.key](value, form, parsedKey.params)
   }, true)
 }
