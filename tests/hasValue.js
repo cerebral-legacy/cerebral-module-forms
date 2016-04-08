1cf22e52ace4fb1154ade7edc2ab99762519b5c3
Javaprogrammer <info@edgesoft.se>
2016-04-08 17:01:47 +0200

    test: added test for hasValue

---
+++ tests/hasValue.js
@@ -0,0 +1,34 @@
+var hasValueHelper = require('../helpers/hasValue')
+var Form = require('../Form')
+
+module.exports = {
+    setUp: function (callback) {
+        this.form = Form({
+           name: {
+             value: [],
+             isValue: ['minLength:3']
+           },
+           name2: {
+              value: [
+                  {},{},{}
+              ],
+              isValue: ['minLength:3'] 
+           }
+        });
+        callback();
+        
+    },
+    tearDown: function (callback) {
+        callback();
+    },
+    
+    testIsValueWithParams: function (test) {
+        var field = this.form['name']
+        var rule = hasValueHelper(this.form, field.value, field.isValue)
+        test.equals(rule, false);
+        field = this.form['name2']
+        rule = hasValueHelper(this.form, field.value, field.isValue)
+        test.equals(rule, true);
+        test.done();
+    }
+};
