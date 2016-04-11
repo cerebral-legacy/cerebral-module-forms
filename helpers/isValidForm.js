1934866a30b63426ff753c7b6f1304177202f59c
Javaprogrammer <info@edgesoft.se>
2016-04-11 11:27:34 +0200

    chore: refactoring out repetitive code
    fix: #2

--- helpers/isValidForm.js
+++ helpers/isValidForm.js
@@ -1,29 +1,6 @@
+var traverseForm = require('./traverseForm')
+var validFormValidator = require('./validFormValidator')
+
 module.exports = function isValidForm (form) {
-  var isValidObject = function (object) {
-    return Object.keys(object).reduce(function (isValid, key) {
-      if (!isValid) {
-        return false
-      }
-
-      if (Array.isArray(object[key])) {
-        return isValidArray(object[key])
-      } else if ('value' in object[key]) {
-        return object[key].isValid && (!object[key].isRequired || (object[key].isRequired && object[key].hasValue))
-      } else {
-        return isValidObject(object[key])
-      }
-    }, true)
-  }
-
-  var isValidArray = function (array) {
-    return array.reduce(function (isValid, object) {
-      if (!isValid) {
-        return false
-      }
-
-      return isValidObject(object)
-    }, true)
-  }
-
-  return isValidObject(form)
+  return traverseForm(form, validFormValidator)
 }
--- helpers/isValidatingForm.js
+++ helpers/isValidatingForm.js
@@ -1,29 +1,6 @@
+var traverseForm = require('./traverseForm')
+var validatingFormValidator = require('./validatingFormValidator')
+
 module.exports = function isValidForm (form) {
-  var isValidObject = function (object) {
-    return Object.keys(object).reduce(function (isValid, key) {
-      if (!isValid) {
-        return false
-      }
-
-      if (Array.isArray(object[key])) {
-        return isValidArray(object[key])
-      } else if ('value' in object[key]) {
-        return object[key].isValidating
-      } else {
-        return isValidObject(object[key])
-      }
-    }, true)
-  }
-
-  var isValidArray = function (array) {
-    return array.reduce(function (isValid, object) {
-      if (!isValid) {
-        return false
-      }
-
-      return isValidObject(object)
-    }, true)
-  }
-
-  return isValidObject(form)
+  return traverseForm(form, validatingFormValidator)
 }
